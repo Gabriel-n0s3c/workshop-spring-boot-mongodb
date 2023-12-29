8adfe2f2ae95927cceef796e42e09496f0929a0d
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2023-12-29 16:41:35 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/nosec/workshopmongo/config/Instatiation.java
@@ -0,0 +1,30 @@
+package com.nosec.workshopmongo.config;
+
+import com.nosec.workshopmongo.domain.User;
+import com.nosec.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instatiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.deleteAll();
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+
+    }
+}
