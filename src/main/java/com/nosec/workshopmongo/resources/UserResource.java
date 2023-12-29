026d07f871a91d4c386344a0e3b70670959b87c5
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2023-12-29 17:04:22 -0300

    Obtendo um usuário por id

--- src/main/java/com/nosec/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nosec/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.nosec.workshopmongo.dto.UserDTO;
 import com.nosec.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +22,18 @@ public class UserResource {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll() {
-        
+
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 
-
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = service.findById(id);
+        UserDTO userDto = new UserDTO(user);
+
+        return ResponseEntity.ok().body(userDto);
     }
 }
--- src/main/java/com/nosec/workshopmongo/services/UserService.java
+++ src/main/java/com/nosec/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.nosec.workshopmongo.services;
 
 import com.nosec.workshopmongo.domain.User;
 import com.nosec.workshopmongo.repository.UserRepository;
+import com.nosec.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return this.userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
