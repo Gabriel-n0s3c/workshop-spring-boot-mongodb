656b11766fd3e6eea778c2cd92c06bdf5732e53c
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2023-12-29 16:37:46 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/nosec/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nosec.workshopmongo.repository;
+
+import com.nosec.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
