2dc5f50446123e397c7f394ee79508311fc8f8c8
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2024-01-02 17:48:13 -0300

    Consulta simples com query methods

--- src/main/java/com/nosec/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nosec/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.nosec.workshopmongo.resources;
 import com.nosec.workshopmongo.domain.Post;
 import com.nosec.workshopmongo.domain.User;
 import com.nosec.workshopmongo.dto.UserDTO;
+import com.nosec.workshopmongo.resources.util.URL;
 import com.nosec.workshopmongo.services.PostService;
 import com.nosec.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,13 @@ public class PostResource {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> obj = service.findByTitle(text);
+        return ResponseEntity.ok().body(obj);
+    }
+
 
 }
