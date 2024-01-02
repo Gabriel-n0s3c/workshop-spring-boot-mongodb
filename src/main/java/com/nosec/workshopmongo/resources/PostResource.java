215058ae340ad22bd8f4c56be5980c76d4319171
Gabriel N0s3c <gabriel.marques.o.silva@gmail.com>
2024-01-02 18:16:35 -0300

    Consulta com varios criterios

--- src/main/java/com/nosec/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nosec/workshopmongo/resources/PostResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,6 +35,26 @@ public class PostResource {
         List<Post> obj = service.findByTitle(text);
         return ResponseEntity.ok().body(obj);
     }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate
+            ){
+
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+
+        List<Post> obj = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+
+
 
 
 }
